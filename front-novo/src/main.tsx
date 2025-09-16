a3711710e677c83d011a165cb21068665700c71f
Jose Carlo <josecarlos.dias@estudante.ufjf.br>
2025-09-16 04:55:08 -0300

    chore(wallet): comenta integração com carteira/Freighter e usa DEMO_WALLET
    - Desabilita SorobanProvider no main
    - Comenta hooks de carteira (useWallet)
    - Substitui address por DEMO_WALLET em payloads

--- front-novo/src/main.tsx
+++ front-novo/src/main.tsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import SorobanProvider from './components/SorobanProvider.tsx'
+// import SorobanProvider from './components/SorobanProvider.tsx'
 import { LanguageProvider } from './i18n/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SorobanProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </SorobanProvider>
+    {/* SorobanProvider desabilitado para acesso sem carteira */}
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 )
