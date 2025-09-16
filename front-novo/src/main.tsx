8afe3f1d2c407485668ffbeb190cf63c0f79adb4
Jose Carlo <josecarlos.dias@estudante.ufjf.br>
2025-09-16 05:01:20 -0300

    chore(wallet): comenta conexão de carteira (Freighter) sem remover código; navegação sem login

--- front-novo/src/main.tsx
+++ front-novo/src/main.tsx
@@ -2,15 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import SorobanProvider from './components/SorobanProvider.tsx'
+// import SorobanProvider from './components/SorobanProvider.tsx' // Comentado para acessar sem carteira
 import { LanguageProvider } from './i18n/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SorobanProvider>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </SorobanProvider>
+    {/*
+      Bloco original com carteira (Freighter/Soroban). Reative quando quiser exigir login de carteira:
+      <SorobanProvider>
+        <LanguageProvider>
+          <App />
+        </LanguageProvider>
+      </SorobanProvider>
+    */}
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
 )
